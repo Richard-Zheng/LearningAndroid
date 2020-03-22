2b7b519160ee4c2568edadb62bd67eb5a5e5363e
richard-zheng <1919500872@qq.com>
2020-03-22 18:54:54 +0800

    Added the entrance of TemplateBasicActivity

--- app/src/main/java/com/richard/learningandroid/MainActivity.java
+++ app/src/main/java/com/richard/learningandroid/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ScrollingTextActivity.class);
         startActivity(intent);
     }
+
+    public void startTemplateBasicActivity(View view) {
+        Intent intent = new Intent(this, TemplateBasicActivity.class);
+        startActivity(intent);
+    }
 }
